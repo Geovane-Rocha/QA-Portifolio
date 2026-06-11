4ab3ea0f0714f654ab9ddd959e5bcc76ff16e856
GeovaneQA <110861856+Geovane-Rocha@users.noreply.github.com>
2026-06-11 10:22:06 -0300

    Update Test_login.py

--- Automação/Test_login.py
+++ Automação/Test_login.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-
+ 
 def test_login_valido():
     # Abre o navegador
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
